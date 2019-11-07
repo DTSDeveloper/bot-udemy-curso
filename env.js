e61aa3bac7be020486d3c39faafcc463bf6653ce
unknown <danilo.turqueti.developer@gmail.com>
2019-11-07 09:58:30 -0300

    error handler is not function

---
+++ env.js
@@ -0,0 +1,7 @@
+const token = '908924333:AAEnSCSwiZpWsyJUfdvEEa0wJyCR9u-VQos';
+
+module.exports = {
+    token,
+    apiUrl: `https://api.telegram.org/bot${token}`,
+    apiFileUrl: `https://api.telegram.org/file/bot${token}`
+}
